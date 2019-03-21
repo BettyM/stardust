f721fb19be37676f2940fe0d57c3e137f00b4d8e
BettyM <bettymp81@gmail.com>
2019-03-20 17:55:58 -0700

    Update index file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+//import logo from './logo.svg'
+import './styles/index.css'
+import Stardust from './components/Dashboard'
 
+          //<img src={logo} className="App-logo" alt="logo" />
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Stardust />
         </header>
       </div>
     );
