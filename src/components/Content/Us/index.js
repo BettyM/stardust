8d09a4830111fdb400b7b8f6c7057b48b5206048
BettyM <bettymp81@gmail.com>
2019-04-05 14:39:28 -0700

    Add Quienes somos content

--- src/components/Content/Us/index.js
+++ src/components/Content/Us/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import MercuryImg from '../../../static/images/Mercury-33.jpg'
 
 const Us = () => {
   return (
-    <div>
-      <h1>Quienes somos...</h1>
-    </div>
+    <Grid item xs={12}>
+      <div className="grid-root">
+        <Card className="card">
+          <CardMedia
+            component="img"
+            alt="mercury"
+            className="card-media"
+            height="140"
+            image={MercuryImg}
+            title="Mercury Team"
+          />
+          <CardContent>
+            <Typography component="p" align="justify" variant="h6">
+              Somos Mercury Project, una escudería inspirada por la ingeniería, ciencia y tecnología. Donde convergemos todos los integrantes aportando nuestras habilidades, actitudes y energía para lograr con éxito el desafío de F1 In Schools México.
+          </Typography>
+        </CardContent>
+        </Card>
+      </div>
+    </Grid>
   )
 }
 
