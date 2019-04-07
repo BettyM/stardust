d23adfc51f6d8e03bc7c01e95badd3d05965c75e
BettyM <bettymp81@gmail.com>
2019-04-07 12:37:48 -0700

    Add Extra content

--- src/components/Content/Extras/index.js
+++ src/components/Content/Extras/index.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import videos from './videos'
 
-const Extras = () => {
-  return (
-    <div>
-      <h1>Extras...</h1>
-    </div>
-  )
+export default class Extras extends Component {
+  render() {
+    return (
+      <Grid item xs={12}>
+        <div className="grid-root">
+          <GridList cellHeight={160} className="grid-list" cols={1}>
+            {videos.map(tile => (
+              <GridListTile
+                key={tile.img}
+                cols={tile.cols || 1}
+                className="grid-tile"
+              >
+                <video
+                  controls
+                  src={tile.img}
+                  alt={tile.title}
+                  className="content-image"
+                >
+                  <source
+                    type="video/mp4"
+                    src={tile.img}
+                  />
+                </video>
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      </Grid>
+    )
+  }
 }
-
-export default Extras
---
+++ src/components/Content/Extras/videos.js
@@ -0,0 +1,17 @@
+import Video1 from '../../../static/videos/MercuryProject.mp4'
+import Video2 from '../../../static/videos/CUENTOMERCURYRACE.mp4'
+
+const videos = [
+  {
+    img: Video1,
+    title: 'Video',
+    author: 'author',
+  },
+  {
+    img: Video2,
+    title: 'Video',
+    author: 'author',
+  },
+]
+
+export default videos
