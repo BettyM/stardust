9784b3b354527f21d87315b3225a3136f63b26bd
BettyM <bettymp81@gmail.com>
2019-04-08 09:26:38 -0700

    Adjust Reaction Game style

--- src/components/Content/ReactionGame/index.js
+++ src/components/Content/ReactionGame/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
-//import Camera from 'react-html5-camera-photo'
-//import 'react-html5-camera-photo/build/css/index.css'
-//import PhotoCapture from '../PhotoCapture'
+import Grid from '@material-ui/core/Grid'
 
 const ReactionGame = () => {
   return (
-    <div>
-      <iframe src="https://f1-start.glitch.me/" height="800" width="1000" title="game" />
-    </div>
+    <Grid item xs={12}>
+      <div>
+        <iframe
+          src="https://f1-start.glitch.me/"
+          height="800"
+          width="1000"
+          title="game"
+          className="iframe"
+        />
+      </div>
+    </Grid>
   )
 }
 
