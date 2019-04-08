d616ec0c9c7b427d6029c4e5030dc6aa488e9387
BettyM <bettymp81@gmail.com>
2019-04-08 09:14:24 -0700

    Add History content

---
+++ src/components/Content/History/data.js
@@ -0,0 +1,104 @@
+import Image1 from '../../../static/images/historia/Mercury_Seven_Astronauts.jpg'
+import Image2 from '../../../static/images/historia/C22S15F2.jpg'
+import Image3 from '../../../static/images/historia/FB_IMG_1.jpg'
+import Image4 from '../../../static/images/historia/FB_IMG_2.jpg'
+import Image5 from '../../../static/images/historia/FB_IMG_3.jpg'
+import Image6 from '../../../static/images/historia/FB_IMG_4.jpg'
+import Image7 from '../../../static/images/historia/FB_IMG_5.jpg'
+import Image8 from '../../../static/images/historia/Mercury_Alas6.jpg'
+import Image9 from '../../../static/images/historia/JerrieCobb_MercuryCapsule.jpg'
+//import Image10 from '../../../static/images/historia/Project_Mercury_Astronauts.jpg'
+import Image11 from '../../../static/images/historia/Image1.jpeg'
+
+const history1 = "Proyecto Mercurio fue el primer programa espacial tripulado de los Estados Unidos, desarrollado entre 1961 y 1963 en el marco de la carrera espacial. Los inicios del proyecto se remontan al 7 de octubre de 1958, un año y tres días después de que la Unión Soviética pusiera al primer satélite en el espacio, en órbita alrededor de la Tierra, el Sputnik 1."
+const history2 ="La misión e idea del Proyecto Mercurio, consistía en enviar un hombre a la órbita de la tierra, que viviera la experiencia estimada, regresara con vida a la tierra y todo eso antes que la USRR."
+const history3 = "Dicho esto, la USRR estaba arrasando con todos los logros espaciales, desde el armamento, hasta ser el primer gobierno en el mundo en enviar vida al espacio; Ham el mono. Por ende , USA no se quedaría con los brazos cruzados, traslado a un equipo a Guaymas, Empalme, Sonora."
+const history4 = "Al oriente de la ciudad de Empalme, de 1961 a 1970 funciono una estación rastreadora de lo que ahora se reconoce con las siglas NASA. Se denomina “Proyecto Mercurio”, en alusión al programa espacial de EE UU, cuya visión era el hombre de la luna. Esta estación, que por cierto fue la única que se construyó en el país, fue utilizada también por los proyectos “Apollo” y “Geminis”. Estas instalaciones rastreadoras, que igual existían otras en distintos países, tenían la función de localizar las naves espaciales y comunicarse con los astronautas."
+
+const data = [
+  {
+    img: Image1,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: Image4,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: Image7,
+    title: 'Image',
+    author: 'author',
+  },
+  /*
+  {
+    img: Image10,
+    title: 'Image',
+    author: 'author',
+    style: 'content-image-margin',
+  },
+  */
+  {
+    img: Image3,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: Image5,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: Image6,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: Image2,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: Image9,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: Image8,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: history1,
+    title: 'Text',
+    author: 'author',
+    cols: 3,
+  },
+  {
+    img: history2,
+    title: 'Text',
+    author: 'author',
+    cols: 3,
+  },
+  {
+    img: history3,
+    title: 'Text',
+    author: 'author',
+    cols: 3,
+  },
+  {
+    img: history4,
+    title: 'Text',
+    author: 'author',
+    cols: 3,
+  },
+  {
+    img: Image11,
+    title: 'Image',
+    author: 'author',
+    cols: 3,
+  },
+]
+
+export default data
--- src/components/Content/History/index.js
+++ src/components/Content/History/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import Typography from '@material-ui/core/Typography'
+import data from './data'
 
-const History = () => {
-  return (
-    <div>
-      <h1>History...</h1>
-    </div>
-  )
+export default class History extends Component {
+  render() {
+    return (
+      <Grid item xs={12}>
+        <div className="grid-root">
+          <GridList cellHeight={160} className="grid-list" cols={3}>
+            {data.map(tile => (
+              <GridListTile
+                key={tile.img}
+                cols={tile.cols || 1}
+                className="grid-tile"
+              >
+                {tile.title === "Image" &&
+                <img
+                  src={tile.img}
+                  alt={tile.title}
+                  className={tile.style || "content-image"}
+                />}
+                {tile.title === "Text" &&
+                  <Typography component="p" align="justify" variant="h6">
+                    {tile.img}
+                  </Typography>
+                }
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      </Grid>
+    )
+  }
 }
-
-export default History
