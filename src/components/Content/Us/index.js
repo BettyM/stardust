f8d6dd8ecc590b347031337754f15ab9c224fbc9
BettyM <bettymp81@gmail.com>
2019-06-04 21:22:17 -0700

    Update mercury team photo

--- src/components/Content/Us/index.js
+++ src/components/Content/Us/index.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import MercuryImg from '../../../static/images/Mercury-33.jpg'
+import MercuryImg from '../../../static/images/mercury.jpg'
 
 const Us = () => {
   return (
