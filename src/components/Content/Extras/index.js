58c80f099379cc459276cef73e21bff9a5631e9b
BettyM <bettymp81@gmail.com>
2019-06-04 23:21:44 -0700

    Add text to extras

--- src/components/Content/Extras/index.js
+++ src/components/Content/Extras/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
+import Typography from '@material-ui/core/Typography'
 import videos from './videos'
 
 export default class Extras extends Component {
@@ -16,6 +17,7 @@ export default class Extras extends Component {
                 cols={tile.cols || 1}
                 className="grid-tile"
               >
+                {tile.title === "Video" &&
                 <video
                   controls
                   src={tile.img}
@@ -26,7 +28,17 @@ export default class Extras extends Component {
                     type="video/mp4"
                     src={tile.img}
                   />
-                </video>
+                </video>}
+                {tile.title === "Text" &&
+                  <Typography
+                    component="span"
+                    align="justify"
+                    variant="h6"
+                    className={tile.style || "phrase"}
+                  >
+                    {tile.img}
+                  </Typography>
+                }
               </GridListTile>
             ))}
           </GridList>
--- src/components/Content/Extras/videos.js
+++ src/components/Content/Extras/videos.js
@@ -2,6 +2,9 @@ import Video1 from '../../../static/videos/MercuryProject.mp4'
 import Video2 from '../../../static/videos/CUENTOMERCURYRACE.mp4'
 import Video3 from '../../../static/videos/extra3.mp4'
 
+const text1 = "'Su motivación era más grande que su miedo al fracaso'"
+const text2 = "-Mercury Project"
+
 const videos = [
   {
     img: Video1,
@@ -12,6 +15,17 @@ const videos = [
     img: Video2,
     title: 'Video',
     author: 'author',
+  },
+  {
+    img: text1,
+    title: 'Text',
+    author: 'author',
+  },
+  {
+    img: text2,
+    title: 'Text',
+    author: 'author',
+    style: 'phrase-right'
   },
   {
     img: Video3,
