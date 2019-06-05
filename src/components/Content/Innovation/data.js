e89ab96f3d42eb7d15c6ede317823ee95082dfa8
BettyM <bettymp81@gmail.com>
2019-06-04 22:39:26 -0700

    Update Ingenieria

--- src/components/Content/Innovation/data.js
+++ src/components/Content/Innovation/data.js
@@ -1,9 +1,9 @@
 import Image1 from '../../../static/images/ingenieria/elotitoflow.png'
-import Image2 from '../../../static/images/ingenieria/elotitoflow2.png'
-import Image3 from '../../../static/images/ingenieria/elotitoflow3.png'
+//import Image2 from '../../../static/images/ingenieria/elotitoflow2.png'
+//import Image3 from '../../../static/images/ingenieria/elotitoflow3.png'
 import Image4 from '../../../static/images/ingenieria/cnc1.jpg'
-import Image5 from '../../../static/images/ingenieria/cnc2.jpg'
-import Video from '../../../static/videos/Ingenieria.mp4'
+//import Image5 from '../../../static/images/ingenieria/cnc2.jpg'
+//import Video from '../../../static/videos/Ingenieria.mp4'
 
 const data = [
   {
@@ -12,6 +12,7 @@ const data = [
     author: 'author',
     cols: 2,
   },
+  /*
   {
     img: Image2,
     title: 'Image',
@@ -24,12 +25,14 @@ const data = [
     author: 'author',
     cols: 2,
   },
+  */
   {
     img: Image4,
     title: 'Image',
     author: 'author',
     cols: 2,
   },
+  /*
   {
     img: Image5,
     title: 'Image',
@@ -43,6 +46,7 @@ const data = [
     author: 'author',
     cols: 2,
   },
+  */
 ]
 
 export default data
