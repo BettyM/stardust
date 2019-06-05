1745d98b7f17cf75809c42a90d25be8cf4e4b19d
BettyM <bettymp81@gmail.com>
2019-06-04 21:29:11 -0700

    Add extra video

--- src/components/Content/Extras/videos.js
+++ src/components/Content/Extras/videos.js
@@ -1,5 +1,6 @@
 import Video1 from '../../../static/videos/MercuryProject.mp4'
 import Video2 from '../../../static/videos/CUENTOMERCURYRACE.mp4'
+import Video3 from '../../../static/videos/extra3.mp4'
 
 const videos = [
   {
@@ -9,6 +10,11 @@ const videos = [
   },
   {
     img: Video2,
+    title: 'Video',
+    author: 'author',
+  },
+  {
+    img: Video3,
     title: 'Video',
     author: 'author',
   },
